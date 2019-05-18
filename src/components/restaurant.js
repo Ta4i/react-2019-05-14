790f7b2d1b25332756e814691409a85e92d739b2
egorro <darktitan125@gmail.com>
2019-05-18 23:21:16 +0300

    added star component

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,20 +1,31 @@
 import React, { PureComponent } from "react";
 import RestaurantMenu from "./restaurant-menu";
+import Rate from "antd/lib/rate";
 
 class Restaurant extends PureComponent {
   render() {
-    const { image, name, menu, isMenuOpen } = this.props;
+    const { image, name, menu, reviews, isMenuOpen } = this.props;
+    const averageRating = this.calculateAverageRating(reviews);
 
     return (
       <div>
         <img src={image} width={64} height={64} alt={name} />
         <h3>{name}</h3>
+        <Rate allowHalf defaultValue={averageRating} />
+        <br />
         <button onClick={this.handleToggleOpenClick}>
           {isMenuOpen ? "Close menu" : "Open menu"}
         </button>
         {isMenuOpen ? <RestaurantMenu menu={menu} /> : null}
       </div>
     );
+  }
+
+  calculateAverageRating(reviews) {
+    const ratingSum = reviews.reduce((sum, review) => {
+      return sum + review.rating;
+    }, 0);
+    return ratingSum / reviews.length;
   }
 
   handleToggleOpenClick = () => {

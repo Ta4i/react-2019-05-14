a70c81f3aa93c04300106727ff7a1725b509d3aa
pelesesenko <kw04@i.ua>
2019-05-23 21:50:49 +0300

    HT2

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -4,6 +4,7 @@ import { List, Avatar, Button } from "antd";
 import AverageRating from "./average-rating";
 import ReviewList from "./review-list";
 import { toggleVisibility } from "../decorators/toggleVisibility";
+import PropTypes from "prop-types";
 
 class Restaurant extends PureComponent {
   state = {
@@ -17,7 +18,7 @@ class Restaurant extends PureComponent {
 
   render() {
     const {
-      id,
+      //      id,
       image,
       name,
       menu,
@@ -35,7 +36,10 @@ class Restaurant extends PureComponent {
           style={{ paddingLeft: "8px" }}
           actions={[
             <AverageRating reviews={reviews} />,
-            <Button onClick={toggleVisibility}>
+            <Button
+              data-automation-id="toggle-reviews"
+              onClick={toggleVisibility}
+            >
               {isReviewOpen ? "Hide reviews" : "Show reviews"}
             </Button>,
             <Button
@@ -61,5 +65,17 @@ class Restaurant extends PureComponent {
     this.props.toggleOpenMenu(this.props.id);
   };
 }
+
+Restaurant.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  location: PropTypes.objectOf(PropTypes.number).isRequired,
+  image: PropTypes.string.isRequired,
+  menu: PropTypes.arrayOf(PropTypes.object).isRequired,
+  reviews: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isMenuOpen: PropTypes.bool.isRequired,
+  isOpen: PropTypes.bool,
+  toggleVisibility: PropTypes.func.isRequired
+};
 
 export default toggleVisibility(Restaurant);

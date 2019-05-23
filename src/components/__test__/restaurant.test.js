f99bd1a718f2b937de20f0a8cb92a52007ea14fb
Kirill Lappo <kirill.lappo@oxagile.com>
2019-05-23 21:15:43 +0300

    addded one more test; common steps were moved to upper context

--- src/components/__test__/restaurant.test.js
+++ src/components/__test__/restaurant.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Restaurant from "../restaurant";
-import { mount, render, configure } from "enzyme";
+import { mount, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
@@ -28,9 +27,46 @@ const restaurantWithReviewModel = {
   ]
 };
 
-describe("when click on Open Review", () => {
+const restaurantWithoutReviewModel = {
+  id: "RestauranWithReview",
+  name: "RestauranWithReview",
+  location: {},
+  image: "",
+  menu: [],
+  reviews: []
+};
+
+describe("restaurant with menu and click on Open Review", () => {
+  const wrapper = mount(<Restaurant {...restaurantWithReviewModel} />);
+  const findActualReviews = () => wrapper.find('[data-aid*="review-"]');
+  const clickOnToggleReviewsButton = () =>
+    wrapper
+      .find('[data-aid="toggle-reviews"]')
+      .first()
+      .simulate("click");
+
   it("should open review list with correct amount of reviews", () => {
-    const wrapper = mount(<Restaurant {...restaurantWithReviewModel} />);
+    clickOnToggleReviewsButton();
+
+    const actualReviews = findActualReviews();
+
+    expect(actualReviews.length).toEqual(
+      restaurantWithReviewModel.reviews.length
+    );
+  });
+
+  it("should close review list after second click", () => {
+    clickOnToggleReviewsButton();
+
+    const actualReviews = findActualReviews();
+
+    expect(actualReviews.length).toEqual(0);
+  });
+});
+
+describe("restaurant without menu and click on Open Review", () => {
+  it("has no reviews in a list", () => {
+    const wrapper = mount(<Restaurant {...restaurantWithoutReviewModel} />);
 
     wrapper
       .find('[data-aid="toggle-reviews"]')
@@ -40,7 +76,7 @@ describe("when click on Open Review", () => {
     const actualReviews = wrapper.find('[data-aid*="review-"]');
 
     expect(actualReviews.length).toEqual(
-      restaurantWithReviewModel.reviews.length
+      restaurantWithoutReviewModel.reviews.length
     );
   });
 });

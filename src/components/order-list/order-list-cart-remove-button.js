57109f6f7d5e5b203775af2962910e0d70442fba
Kirill <dnaplayer@yandex.by>
2019-05-26 16:50:47 +0300

    shared controls

---
+++ src/components/order-list/order-list-cart-remove-button.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Icon, Button } from "antd";
+import { connect } from "react-redux";
+import { removeFromCart } from "../../ac";
+
+class OrderListItemRemoveButton extends Component {
+  render() {
+    const { itemId, removeFromCart } = this.props;
+    return (
+      <Button onClick={() => removeFromCart(itemId)}>
+        <Icon type="delete" />
+      </Button>
+    );
+  }
+}
+
+export default connect(
+  (state, props) => props,
+  {
+    removeFromCart
+  }
+)(OrderListItemRemoveButton);

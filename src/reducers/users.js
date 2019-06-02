a8e3eb24c113b44aa2e1c774b654df09cfb810bd
Kirill <dnaplayer@yandex.by>
2019-06-02 19:01:22 +0300

    added one more reducer checlk

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,23 @@
 import { normalizedUsers } from "../fixtures";
-import { ADD_USER } from "./../constants/users";
+import { ADD_REVIEW } from "./../constants/reviews";
+import _ from "lodash";
 
 export default (state = normalizedUsers, action) => {
   switch (action.type) {
-    case ADD_USER: {
-      let review = action.payload;
-      return [...state, review];
+    case ADD_REVIEW: {
+      let { userId, userName } = action.payload;
+      let user = _.find(state, u => u.id === userId);
+      if (user) {
+        return state;
+      }
+
+      return [
+        ...state,
+        {
+          id: userId,
+          name: userName
+        }
+      ];
     }
     default:
       return state;

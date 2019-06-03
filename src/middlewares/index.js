a9dac905ee7239c198c64862ac2a9dddfe9cd0e0
Aleksandr Oksen <aledor777@gmail.com>
2019-06-03 20:38:02 +0300

    fix hw4

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,5 @@
 import uuidv4 from "uuid/v4";
+import { ADD_REVIEW } from "../constants";
 
 export let logger = store => next => action => {
   // console.log("before", store.getState());
@@ -8,69 +9,71 @@ export let logger = store => next => action => {
 };
 
 export let generateId = store => next => action => {
-  let { name, value, review, restaurantId } = action.payload.review;
-  let { reviews, users, restaurants, ...state } = store.getState();
+  if (action.type === ADD_REVIEW) {
+    let { name, value, review, restaurantId } = action.payload.review;
+    let { reviews, users, restaurants } = store.getState();
 
-  let oldUser = users.find(
-    user => user.name.toLowerCase() === name.toLowerCase()
-  );
-  let restaurant = restaurants.find(
-    restaurant => restaurant.id === restaurantId
-  );
-  let idxRestaurant = restaurants.findIndex(
-    restaurant => restaurant.id === restaurantId
-  );
+    let oldUser = users.find(
+      user => user.name.toLowerCase() === name.toLowerCase()
+    );
+    let restaurant = restaurants.find(
+      restaurant => restaurant.id === restaurantId
+    );
+    let idxRestaurant = restaurants.findIndex(
+      restaurant => restaurant.id === restaurantId
+    );
 
-  let generatedNewReviewId = uuidv4();
+    let generatedNewReviewId = uuidv4();
 
-  let updatedRestaurant = {
-    ...restaurant,
-    reviews: [...restaurant.reviews, generatedNewReviewId]
-  };
-
-  let newUser;
-  let updatedUsers;
-  let newReview = {
-    id: generatedNewReviewId,
-    rating: value,
-    text: review
-  };
-
-  if (oldUser) {
-    newReview = {
-      ...newReview,
-      userId: oldUser.id
+    let updatedRestaurant = {
+      ...restaurant,
+      reviews: [...restaurant.reviews, generatedNewReviewId]
     };
 
-    updatedUsers = [...users];
-  } else {
-    let newUserId = uuidv4();
-
-    newUser = {
-      id: newUserId,
-      name
+    let newUser;
+    let updatedUsers;
+    let newReview = {
+      id: generatedNewReviewId,
+      rating: value,
+      text: review
     };
 
-    newReview = {
-      ...newReview,
-      userId: newUserId
-    };
+    if (oldUser) {
+      newReview = {
+        ...newReview,
+        userId: oldUser.id
+      };
 
-    updatedUsers = [...users, newUser];
+      updatedUsers = [...users];
+    } else {
+      let newUserId = uuidv4();
+
+      newUser = {
+        id: newUserId,
+        name
+      };
+
+      newReview = {
+        ...newReview,
+        userId: newUserId
+      };
+
+      updatedUsers = [...users, newUser];
+    }
+
+    let updatedReviews = [...reviews, newReview];
+    let updatedRestaurants = [
+      ...restaurants.slice(0, idxRestaurant),
+      updatedRestaurant,
+      ...restaurants.slice(idxRestaurant + 1)
+    ];
+
+    action.payload.result = {
+      users: updatedUsers,
+      restaurants: updatedRestaurants,
+      reviews: updatedReviews
+    };
   }
-
-  let updatedReviews = [...reviews, newReview];
-  let updatedRestaurants = [
-    ...restaurants.slice(0, idxRestaurant),
-    updatedRestaurant,
-    ...restaurants.slice(idxRestaurant + 1)
-  ];
-
-  action.payload.result = {
-    users: updatedUsers,
-    restaurants: updatedRestaurants,
-    reviews: updatedReviews
-  };
 
   next(action);
 };

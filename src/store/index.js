89caac03021dbfe396216d49f76d0da367cac142
Anatoly Bayusov <woland.rtf@gmail.com>
2019-06-07 02:16:05 +0600

    enable logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import reducer from "../reducers";
-//import logger from "../middlewares/logger";
+import logger from "../middlewares/logger";
 import generateId from "../middlewares/generate-id";
 import provideUserId from "../middlewares/provide-user-id";
 import api from "../middlewares/api";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const enhancer = composeWithDevTools(
-  applyMiddleware(api, generateId, provideUserId /*logger*/)
+  applyMiddleware(api, generateId, provideUserId, logger)
 );
 
 const store = createStore(reducer, enhancer);

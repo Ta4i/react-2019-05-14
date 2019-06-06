e3c89c9facc908fc4a215f787cc6e9d4f2c54307
ilnaz.akpambekov <akpambekov.rabota@yandex.ru>
2019-06-06 20:49:03 +0300

    update selectors, reducers and middlewares

--- src/reducers/dishes.js
+++ src/reducers/dishes.js
@@ -1,5 +1,31 @@
-import { normalizedDishes } from "../fixtures";
+import {FAIL, LOAD_DISHES, START, SUCCESS } from "../constants";
+import { fromJS } from "immutable";
 
-export default (dishesState = normalizedDishes, action) => {
-  return dishesState;
+const initialState = {
+  loaded: false,
+  loading: false,
+  error: null,
+  entities: []
+};
+
+export default (dishesState =  fromJS(initialState), action) => {
+  switch (action.type) {
+    case LOAD_DISHES + START: {
+      return dishesState.set("loading", true);
+    }
+    case LOAD_DISHES + SUCCESS: {
+      return dishesState
+        .set("entities", fromJS(action.response))
+        .set("loading", false)
+        .set("loaded", true);
+    }
+    case LOAD_DISHES + FAIL: {
+      return dishesState
+        .set("loading", false)
+        .set("loaded", false)
+        .set("error", action.error);
+    }
+    default:
+      return dishesState;
+  }
 };
